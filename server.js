6ce7bc119f60c0aab7d499a21ab577e1f5b0dd7d
Larissa Rabello <lari.rabello@gmail.com>
2016-04-21 22:06:11 -0300

    files and folders were reordered and ejs was included

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var app = express();
-
 var server = require("http").Server(app);
+var r = require("rethinkdb");
 
-var io = require("socket.io")(server);
+//var io = require("socket.io")(server);
+
+app.use("/public", express.static('public'));
+app.set("view engine", 'ejs');
 
 app.get("/", function(req, res) {
-  res.send("Hello");
+  r.connect({host:"localhost", port: 28015}, function(err, conn) {
+     r.db("test").table("food").sample(2).run(conn, function(error, cursor) {
+       cursor.toArray(function(error, results) {
+           if (conn != null) conn.close();
+           res.render('pages/index', {
+             foodlist: results,
+           });
+       });
+     });
+  });
 });
 
 server.listen(3000, function() {
