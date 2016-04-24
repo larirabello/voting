3a9beab656002e21a05368e4081507760a746693
Larissa Rabello <lari.rabello@gmail.com>
2016-04-23 23:06:37 -0300

    implemented voting

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var r = require("rethinkdb");
+var bodyParser = require("body-parser");
 
 //var io = require("socket.io")(server);
 
 app.use("/public", express.static('public'));
 app.set("view engine", 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
 
 app.get("/", function(req, res) {
   r.connect({host:"localhost", port: 28015}, function(err, conn) {
@@ -19,6 +22,16 @@ app.get("/", function(req, res) {
        });
      });
   });
+});
+
+app.post("/vote", function(req,res) {
+
+  r.connect({host:"localhost", port: 28015}, function(err, conn) {
+    r.db("test").table("food").get(req.body.id).update({votes: r.row('votes').add(1).default(1)}).run(conn, function(error, cursor) {
+      if (error) console.log(error);
+    });
+  });
+  res.redirect("/");
 });
 
 server.listen(3000, function() {
